3e1892305ebd82d95465b63f1d228b3380137138
eliezer <eliezer.santos1010@gmail.com>
2023-09-13 06:03:58 -0300

    correção do codigo

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
+	// verifica se o item da lista esta na classe riscado
+	$("#Lista").on("click", "li", function () {
+	// condição se ja estiver riscado 
+		if($(this).hasClass('riscado')){
+			//remove o risco do item 
+			$(this).removeClass('riscado');
+			return
+		}  
+		//adiciona risco a um novo item por vez
+		$(this).addClass('riscado');
+	});
+	
+	//tirando a funçao padrão do botão 
 	$('form').on('submit', function(e) {
 		e.preventDefault();
-
+		//criação de constante chamando o id html
 		const novaTarefa = $('#nova-tarefa').val();
-
-		$(document).ready(function() {
+		//criação e iserção de um novo item da lista 		
 		$("ul").append($('<li>').html(novaTarefa));
-		
-		('click','li',function(){});
-		$('#nova-tarefa').val('');
-		})
+		//quando apertar o botão limpa o input
+		$('#nova-tarefa').val('');	
 	})
 })
